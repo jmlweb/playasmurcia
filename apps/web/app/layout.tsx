15f1d11408530530c86e0903c34f99a5779ddbe2
José Manuel Lucas <josemanuel@jmlweb.es>
2023-06-03 22:03:20 +0200

    feat: initialize project with turborepo

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ apps/web/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1>Playas Murcia</h1>
+  );
+}
