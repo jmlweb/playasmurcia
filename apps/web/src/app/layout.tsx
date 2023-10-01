3a89322e86c0f4a85697b9644f82270b71193102
José Manuel Lucas <josemanuel@jmlweb.es>
2023-10-01 21:39:50 +0200

    feat: upgrade dependencies

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -20,6 +20,7 @@ const libreFranklin = Libre_Franklin({
 });
 
 export const metadata: Metadata = {
+  metadataBase: 'https://www.playasmurcia.com',
   title:
     'PlayasMurcia: La información más completa sobre las playas de la Región de Murcia',
   description: `PlayasMurcia - Descubre tu oasis en las playas de la Costa Cálida.`,
